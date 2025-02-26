5dced4318b40e8cb36c748f5ee0482e2aa566a05
unknown <PJudicial@DESKTOP-I2PTM12.csjar.pj.gob.pe>
2025-02-26 18:08:52 -0500

    integrando css

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { GifExpertApp } from './GifExpertApp'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { GifExpertApp } from './GifExpertApp';
+import './styles.css';
 
 
 
