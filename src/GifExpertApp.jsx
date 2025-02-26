bc722935b4b63b21efbc26b17163b3176f1a6d9b
unknown <PJudicial@DESKTOP-I2PTM12.csjar.pj.gob.pe>
2025-02-26 18:37:04 -0500

    agregando una nueva categoria

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -4,9 +4,16 @@ export const GifExpertApp = () => {
 
     const [categories, setCategories] = useState(['naruto', 'dragon ball']);   //el use state se deja con [] para que sea un arreglo
 
+    const onAddCategory = () => {
+
+        setCategories(prevCate => [...prevCate, 'blade']);
+
+    }
+
     return(
         <>
         <h1>hola mundillo</h1>
+        <button onClick={onAddCategory}>Agregar</button>
         <ol>
             {   //la llaves determinan una expersion de java script
                 categories.map( category => {
