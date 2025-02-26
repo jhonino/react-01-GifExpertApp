cebc23e2376c482772d4d261cb930d349898bda2
unknown <PJudicial@DESKTOP-I2PTM12.csjar.pj.gob.pe>
2025-02-26 18:27:53 -0500

    creando una lista de categorias

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react"
+
 export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['naruto', 'dragon ball']);   //el use state se deja con [] para que sea un arreglo
+
     return(
         <>
         <h1>hola mundillo</h1>
+        <ol>
+            {   //la llaves determinan una expersion de java script
+                categories.map( category => {
+                    return <li Key={category}>{category}</li>
+                })    //el map me permite barrer cada uno de los elementos de los arreglos
+            }
+        </ol>
         </>
     )
 }
